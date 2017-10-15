cd33b7bd4ca99bd5c38ac1a95a25f55eca32d53b
8uddishh <senthil.baliah@gmail.com>
2017-10-15 01:40:55 -0500

    call ExecuteSingleAsync

--- SqlFluent.Web2/Controllers/HomeController.cs
+++ SqlFluent.Web2/Controllers/HomeController.cs
@@ -81,7 +81,7 @@ namespace SqlFluent.Web2.Controllers
                 .Query("Select * from SalesLT.Customer where LastName = @lastname and customerid < @customerId")
                 .Parameter("@customerId", SqlDbType.Int, value: 10)
                 .Parameter("@lastname", SqlDbType.NVarChar, value: "Harris", size: 50)
-                .ExecuteSingle(async reader => new Customer
+                .ExecuteSingleAsync(async reader => new Customer
                 {
                     CustomerId = await reader.GetSafeValueAsync<int>("CustomerId"),
                     Title = await reader.GetSafeValueAsync<string>("Title"),
